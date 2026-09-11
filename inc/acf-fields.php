a806ef0c42eefcb875ff0c123fdfe1ac60452416
Bhakti <patilbhakti2003@gmail.com>
2026-09-11 12:03:57 +0530

    Fix ACF menu dropdown population and optimize responsive footer grid layout

--- inc/acf-fields.php
+++ inc/acf-fields.php
@@ -725,6 +725,14 @@ function custom_theme_populate_nav_menus( $field ) {
 add_filter( 'acf/load_field/name=header_menu', 'custom_theme_populate_nav_menus' );
 add_filter( 'acf/load_field/key=field_header_menu', 'custom_theme_populate_nav_menus' );
 
+// Dynamically populate any footer menu column field (e.g. footer_menu_col_1, footer_menu_col_5, etc.)
+add_filter('acf/load_field', function($field) {
+    if (strpos($field['name'], 'footer_menu_col_') === 0) {
+        return custom_theme_populate_nav_menus($field);
+    }
+    return $field;
+});
+
 
 /**
  * ============================================================
